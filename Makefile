997d5db202c5964864e8827b4716dbf9e3874a19
coding610 <sixten.bohman.08@gmail.com>
2024-01-17 23:50:15 +0100

    preformance check

--- Makefile
+++ Makefile
@@ -13,7 +13,7 @@ OBJ_PATH=build
 CPP_VERSION=20
 
 # Add -O3 for preformance
-CFLAGS := -Wall -g -I$(INC_DIR) -std=c++$(CPP_VERSION) -Wno-logical-op-parentheses -Wno-bitwise-op-parentheses
+CFLAGS := -O3 -Wall -g -I$(INC_DIR) -std=c++$(CPP_VERSION) -Wno-logical-op-parentheses -Wno-bitwise-op-parentheses
 LDFLAGS := -lraylib
 
 #_____________________COMPILE______________________
--- include/raymanager.hpp
+++ include/raymanager.hpp
@@ -9,7 +9,7 @@ class _Ray {
 public:
     Vector2 _end_position;
     float _length;
-    const float _MAX_DEPTH = 2;
+    const float _MAX_DEPTH = 5;
 
     void cast(Grid* grid, Vector2 originpos, float rotation, Vector2 cellsize);
 };
@@ -22,8 +22,8 @@ private:
     std::vector<_Ray*> _rays;
     const Vector2 _resolution;
 
-    const int _number_of_rays = 100;
-    const float _fov = 1; // In radians
+    const int _number_of_rays = 500;
+    const float _fov = 1.1; // In radians
     Vector2 _cellsize;
 
 public:
